379ac63d13228e61ef3935e26fae4180e4ae071b
Tim-Shaffer <tcshaf24@ptd.net>
2020-03-16 17:03:00 -0400

    added the described routes for the html pages

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,2 +1,13 @@
+
+
 // A GET Route to /survey which should display the survey page.
+app.get('/survey', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/survey.html'));
+});
+
+
 // A default, catch-all route that leads to home.html which displays the home page.
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/home.html'));
+});
+
