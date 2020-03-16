e0c9222ff6c882b7e8398d9b783da5ccec3ef117
Tim-Shaffer <tcshaf24@ptd.net>
2020-03-16 17:01:53 -0400

    basic setup for the server requirements to start running

--- server.js
+++ server.js
@@ -1,4 +1,24 @@
+// =============================================================
 // Dependencies
 // =============================================================
 var express = require("express");
 var path = require("path");
+
+// =============================================================
+// Set up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// =============================================================
+// Set up the Express app to handle data parsing
+// =============================================================
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// =============================================================
+// Start the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
