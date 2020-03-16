9bb0987f2f89339f872e9bab1aa367908e27d648
Tim-Shaffer <tcshaf24@ptd.net>
2020-03-16 17:10:06 -0400

    added the described routes for the API

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,27 @@
+// =============================================================
+//  starting point fromActivity 14 
+// =============================================================
+
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-// A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+app.get("/api/friends", function(req, res) {
+    return res.json(friends);
+});
+
+
+// A POST routes /api/friends. This will be used to handle incoming survey results. 
+// This route will also be used to handle the compatibility logic.
+app.post("/api/friends", function(req, res) {
+    // req.body is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newFriend = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newFriend
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newFriend);
+  
+    friendsArray.push(newFriend);
+  
+    res.json(newFriend);
+});
