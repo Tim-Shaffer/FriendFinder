7ea748643dfc9e384e17d898f749a490f832f3e4
Tim-Shaffer <tcshaf24@ptd.net>
2020-03-17 12:36:56 -0400

    made sure the paths were correct and the html files were rendered successfully

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,13 +1,22 @@
+// =============================================================
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// =============================================================
+// Export the API routes so that they can be accessed in other files
+// =============================================================
+module.exports = function(app) {
+
+    // A GET Route to /survey which should display the survey page.
+    app.get('/survey', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/survey.html'));
+    });
 
 
-// A GET Route to /survey which should display the survey page.
-app.get('/survey', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/survey.html'));
-});
+    // A default, catch-all route that leads to home.html which displays the home page.
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/home.html'));
+    });
 
-
-// A default, catch-all route that leads to home.html which displays the home page.
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/home.html'));
-});
-
+};
