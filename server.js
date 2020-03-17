2492005b719a03a72c14302e768af40fbc7dacb5
Tim-Shaffer <tcshaf24@ptd.net>
2020-03-17 12:33:56 -0400

    clean up and test.  works on basic links from each of the options of the home page

--- server.js
+++ server.js
@@ -11,12 +11,6 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // =============================================================
-// Set up the Express app to handle data parsing
-// =============================================================
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// =============================================================
 // Expose the public directory to access the image 
 // =============================================================
 app.use(express.static(path.join(__dirname, './app/public')));
