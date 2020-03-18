6e03d115715e111aa8d8fa84b5268b30e7ee9c63
Tim-Shaffer <tcshaf24@ptd.net>
2020-03-18 16:59:30 -0400

    modified the server, apiRoutes, and survey files to be consistent with the Hot Restaurant solution to test the POST

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,4 @@
 // =============================================================
-//  starting point from Activity 14 
-// =============================================================
-
-// =============================================================
 // Import the list of friend entries
 // =============================================================
 var friends = require('../data/friends.js');
@@ -17,23 +13,18 @@ module.exports = function(app) {
         return res.json(friends);
     });
 
-
     // A POST routes /api/friends. This will be used to handle incoming survey results. 
     // This route will also be used to handle the compatibility logic.
     app.post("/api/friends", function(req, res) {
-        // req.body is equal to the JSON post sent from the user
-        // This works because of our body parsing middleware
+
         var newFriend = req.body;
-    
-        // Using a RegEx Pattern to remove spaces from newFriend
-        // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-        newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
-    
+
         console.log(newFriend);
     
-        friendsArray.push(newFriend);
+        friends.push(newFriend);
     
         res.json(newFriend);
+
     });
 
 };
--- server.js
+++ server.js
@@ -2,28 +2,32 @@
 // Dependencies
 // =============================================================
 var express = require("express");
-var path = require("path");
+// var path = require("path");
 
 // =============================================================
 // Set up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // =============================================================
 // Expose the public directory to access the image 
 // =============================================================
-app.use(express.static(path.join(__dirname, './app/public')));
+// app.use(express.static(path.join(__dirname, './app/public')));
 
 // =============================================================
 // Require the routing modules
 // =============================================================
-require(path.join(__dirname, './app/routing/apiRoutes'))(app);  // tested successfully to return the friends json file
-require(path.join(__dirname, './app/routing/htmlRoutes'))(app);  // tested successfully to return the home html file.
+require('./app/routing/apiRoutes')(app);  // tested successfully to return the friends json file
+require('./app/routing/htmlRoutes')(app);  // tested successfully to return the home html file.
 
 // =============================================================
 // Start the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log("App listening on PORT: " + PORT);
   });
