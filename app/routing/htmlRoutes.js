566087b90a8df86312df2bb5ac5ef5dd4382e764
Tim-Shaffer <tcshaf24@ptd.net>
2020-03-22 13:53:59 -0400

    cleaned up comments in htmlRoutes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,17 +3,22 @@
 // =============================================================
 var path = require("path");
 
-// =============================================================
-// Export the API routes so that they can be accessed in other files
-// =============================================================
+// ==================================================================
+// Export the HTML routes so that they can be accessed in other files
+// ==================================================================
 module.exports = function(app) {
 
+    // =============================================================
     // A GET Route to /survey which should display the survey page.
+    // =============================================================
     app.get('/survey', function(req, res) {
         res.sendFile(path.join(__dirname, '../public/survey.html'));
     });
 
-    // Any other route will drop to this default, catch-all route, that leads to home.html which displays the home page.
+    // =============================================================
+    //  Any other route will drop to this default, catch-all route, 
+    //  that leads to home.html which displays the home page.
+    // =============================================================
     app.use(function(req, res) {
         res.sendFile(path.join(__dirname, '../public/home.html'));
     });
