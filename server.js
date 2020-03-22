5f088cbd13919bfeb3f77aa2b3196733a80dd960
Tim-Shaffer <tcshaf24@ptd.net>
2020-03-22 13:52:02 -0400

    cleaned up comments.  Added the path package back into the server and retested

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 // Dependencies
 // =============================================================
 var express = require("express");
-// var path = require("path");
+var path = require("path");
 
 // =============================================================
 // Set up the Express App
@@ -10,20 +10,22 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+// =============================================================
 // Sets up the Express app to handle data parsing
+// =============================================================
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // =============================================================
 // Expose the public directory to access the image 
 // =============================================================
-app.use(express.static('./app/public'));
+app.use(express.static(path.join(__dirname, './app/public')));
 
 // =============================================================
 // Require the routing modules
 // =============================================================
-require('./app/routing/apiRoutes')(app);  // tested successfully to return the friends json file
-require('./app/routing/htmlRoutes')(app);  // tested successfully to return the home html file.
+require(path.join(__dirname, './app/routing/apiRoutes'))(app); 
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app); 
 
 // =============================================================
 // Start the server to begin listening
